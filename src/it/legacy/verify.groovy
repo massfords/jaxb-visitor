abc16ee87b53ee36fb3df229a3b32ff71a9d635a
Mark Ford <mark@massfords.com>
2022-07-16 13:42:41 -0400

    test: groovy assertions more explicit (#40)

--- src/it/legacy/verify.groovy
+++ src/it/legacy/verify.groovy
@@ -1,11 +1,22 @@
+import com.massfords.jaxb.test.FileAssertion
 import com.massfords.jaxb.test.Fixture
 
 File actualDir = new File(basedir, "target/generated-sources/xjc")
 File expectedDir = new File(basedir, "expected")
 String packageAsPath = "org/example/visitor"
+File packageDir = new File(actualDir, packageAsPath)
 
-assert Fixture.runStandardTests(actualDir, expectedDir, packageAsPath)
+assertions = [
+        "BaseVisitor",
+        "DepthFirstTraverserImpl",
+        "Traverser",
+        "TraversingVisitor",
+        "TraversingVisitorProgressMonitor",
+        "Visitable",
+        "Visitor",
+].collect {name -> return new FileAssertion(name, packageDir) }
 
-File actualFile = new File(actualDir, "org/example/simple/TSimpleRequest.java")
-File expectedFile = new File(expectedDir, "TSimpleRequest.java")
-assert Fixture.actualEqualsExpected(actualFile, expectedFile);
+assertions.add(new FileAssertion("TSimpleRequest", new File(actualDir, "org/example/simple/")))
+
+return Fixture.assertAll(expectedDir, assertions);
+
--- src/it/ogc/verify.groovy
+++ src/it/ogc/verify.groovy
@@ -1,11 +1,14 @@
 import com.massfords.jaxb.test.Fixture
+import com.massfords.jaxb.test.FileAssertion
 
 File actualDir = new File(basedir, "target/generated-sources/xjc")
 File expectedDir = new File(basedir, "expected")
 String packageAsPath = "org/example/visitor"
+File packageDir = new File(actualDir, packageAsPath)
 
-assert Fixture.runSingleTest("DepthFirstTraverserImpl", actualDir, expectedDir, packageAsPath);
+assertions = [
+        new FileAssertion("DepthFirstTraverserImpl", packageDir),
+        new FileAssertion("BinaryComparisonOpType", new File(actualDir, "net/opengis/fes/_2/BinaryComparisonOpType.java")),
+]
 
-assert Fixture.actualEqualsExpected(
-        new File(actualDir, "net/opengis/fes/_2/BinaryComparisonOpType.java"),
-        new File(expectedDir, "BinaryComparisonOpType.java"));
+return Fixture.assertAll(expectedDir, assertions);
